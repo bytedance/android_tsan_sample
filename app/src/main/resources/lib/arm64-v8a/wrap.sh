bb846aac2886441ba93b0b05aa37eeeea6cc5035
Fei Peng <pengfei.02@bytedance.com>
2025-07-31 11:32:15 -0700

    add disable handle signal flags in TSAN_OPTIONS to avoid crash

--- app/src/main/resources/lib/arm64-v8a/wrap.sh
+++ app/src/main/resources/lib/arm64-v8a/wrap.sh
@@ -4,6 +4,6 @@ HERE="$(cd "$(dirname "$0")" && pwd)"
 
 echo "CMD: $@"
 
-export TSAN_OPTIONS="ignore_noninstrumented_modules=1"
+export TSAN_OPTIONS="ignore_noninstrumented_modules=1,handle_segv=0,handle_sigbus=0,handle_sigfpe=0"
 
 $HERE/tsan_loader "$@"
